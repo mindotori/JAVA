bd9142c0996831fdf17c3f3fd8dc2a0e06b7e00e
mindotori <148763440+mindotori@users.noreply.github.com>
2024-08-17 01:15:27 +0900

    JAVA: 입출금 리팩토링

--- src/method/ex/MethodEx3.java
+++ src/method/ex/MethodEx3.java
@@ -6,7 +6,19 @@ public class MethodEx3 {
         int balance = 10000;
 
         //입금 1000원
-        int depositAmount = 2000;
+        int depositAmount = 1000;
+        balance = depositAmount;
+        System.out.println(depositAmount + "원을 입금하였습니다. 현재 잔액: " +balance+"원");
 
+        //출금 2000원
+        int withdrawAmount = 2000;
+        if(balance>=withdrawAmount){
+            balance -= withdrawAmount;
+            System.out.println(withdrawAmount + "원을 출금하였습니다. 현재 잔액: "+balance+"원");
+        } else {
+            System.out.println(withdrawAmount + "원을 출금하려 했으나 잔액이 부족합니다.");
+        }
+
+        System.out.println("최종 잔액: " + balance +"원");
     }
 }
