09da731b265314ce8136875391ff1c0d0945cc0c
mindotori <148763440+mindotori@users.noreply.github.com>
2024-08-17 00:55:31 +0900

    JAVA:메서드 호출로 리팩토링2

---
+++ src/method/ex/MethodEx3.java
@@ -0,0 +1,12 @@
+package method.ex;
+
+public class MethodEx3 {
+
+    public static void main(String[] args) {
+        int balance = 10000;
+
+        //입금 1000원
+        int depositAmount = 2000;
+
+    }
+}
